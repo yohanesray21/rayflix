d20116b344660730d676778ecbbabb66d104e2de
Yohanes Silitonga <yohanesrfsilitonga21@gmail.com>
2022-03-31 22:46:00 +0700

    update

--- src/containers/TvShows/SimilarTvShow/SimilarTvShow.js
+++ src/containers/TvShows/SimilarTvShow/SimilarTvShow.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const SimilarTvShow = () => {
   const [similarTvShow, setSimilarTvShow] = useState({});
 
-  return <div>SimilarTvShow</div>;
+  return <div>SimilarTvShow UPdate</div>;
 };
 
 export default SimilarTvShow;
