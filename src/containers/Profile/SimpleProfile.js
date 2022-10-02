c3e52753c38b1ee7f733d5d2007a97bfe17e5948
bryantobing12 <bryantobing0@gmail.com>
2022-10-02 15:53:55 +0700

    feat: add movie count in profile information

--- src/containers/Profile/SimpleProfile.js
+++ src/containers/Profile/SimpleProfile.js
@@ -6,7 +6,7 @@ import {
   CardContent,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import { idrFormat } from "../../common/currency";
 import GlobalContext from "../../context/GlobalContext";
@@ -14,6 +14,26 @@ import GlobalContext from "../../context/GlobalContext";
 const SimpleProfile = () => {
   const { user } = useContext(GlobalContext);
   const location = useLocation();
+
+  const movieCount = useMemo(() => {
+    const movieOwnedInStorage = sessionStorage.getItem("movieOwned");
+    try {
+      if (movieOwnedInStorage) {
+        const movieIds = JSON.parse(movieOwnedInStorage);
+
+        if (Array.isArray(movieIds)) {
+          return movieIds.length;
+        } else {
+          return [];
+        }
+      } else {
+        return [];
+      }
+    } catch (err) {
+      return 0;
+    }
+  }, []);
+
   return (
     <Box display={location?.pathname === "/profile" ? "none" : ""}>
       <Box width={{ xs: "100%", sm: "50%", md: "30%" }} mt={4}>
@@ -33,6 +53,18 @@ const SimpleProfile = () => {
                   Balance
                 </Typography>
                 <Typography>{idrFormat(user?.balance)}</Typography>
+              </Box>
+              <Box>
+                <Typography variant="caption" fontWeight={700}>
+                  Movie Owned
+                </Typography>
+                {movieCount > 0 ? (
+                  <Typography>
+                    {movieCount} {movieCount === 1 ? "movie" : "movies"}
+                  </Typography>
+                ) : (
+                  <Typography>-</Typography>
+                )}
               </Box>
             </Stack>
           </CardContent>
